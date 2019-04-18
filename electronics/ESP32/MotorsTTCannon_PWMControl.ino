7a2e4ebf0b54c56bd5d10d55014bf96bb0ef3b23
Bernardo Martinez Rocamora Junior <bernardo.rocamora@gmail.com>
2019-04-18 14:36:10 -0300

    Add files via upload

---
+++ electronics/ESP32/MotorsTTCannon_PWMControl.ino
@@ -0,0 +1,60 @@
+// Setting Motor Pins
+const int motor1Pin = 25;
+const int motor2Pin = 26;
+const int motor3Pin = 27;
+
+// Setting PWM properties
+const int freq = 1000;
+
+const int channel1 = 0;
+const int channel2 = 1;
+const int channel3 = 2;
+
+const int resolution = 10;
+
+void setup()
+{
+    // Setting Motor Pins as OUTPUTS
+    pinMode(motor1Pin, OUTPUT);
+    pinMode(motor2Pin, OUTPUT);
+    pinMode(motor3Pin, OUTPUT);
+    
+    // Setting LED PWM functionalities
+    ledcSetup(channel1, freq, resolution);
+    ledcSetup(channel2, freq, resolution);
+    ledcSetup(channel2, freq, resolution);
+    
+    // attach the channel to the GPIO to be controlled
+    ledcAttachPin(motor1Pin, channel1);
+    ledcAttachPin(motor2Pin, channel2);
+    ledcAttachPin(motor3Pin, channel3);
+  
+    // Opens serial channel
+    Serial.begin(115200);
+    // Testing
+    Serial.print("Testing DC Motors...");
+}
+
+void loop()
+{
+    Serial.println("Moving Motor 1 at 25%");
+    ledcWrite(channel1, 256);
+    delay(10000);
+    
+    Serial.println("Moving Motor 1 at 50%");
+    ledcWrite(channel1, 512);
+    delay(10000);
+    
+    Serial.println("Moving Motor 1 at 75%");
+    ledcWrite(channel1, 768);
+    delay(10000);
+    
+    Serial.println("Moving Motor 1 at 100%");
+    ledcWrite(channel1, 1023);
+    delay(10000);
+    
+    Serial.println("Stopping Motor");
+    ledcWrite(channel1, 0);
+    delay(20000);
+
+}
---
+++ electronics/ESP32/pwm_dc_motor_control/pwm_dc_motor_control.ino
@@ -0,0 +1,47 @@
+// the transistor which controls the motor will be attached to digital pin 9
+int motorControl = 9;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // make the transistor's pin an output:
+  pinMode(motorControl, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  analogWrite(motorControl, 167);
+  delay(10000);
+  digitalWrite(LED_BUILTIN,LOW);
+  delay(100);
+  
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  analogWrite(motorControl, 255);
+  delay(10000);
+  digitalWrite(LED_BUILTIN,LOW);
+  delay(100);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
